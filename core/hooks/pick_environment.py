a1fad8c252e41ab3136db328c9e4b428557009ee
Ryan Mayeda <ryanmayeda@rainbowsparkles.local>
2013-05-28 15:55:07 -0700

    Changing from code-only to real-world example
    Since separating the Tank studio install from the project area is now
    built-in functionality as of Tank v0.13, the example configuration for
    working around the lack of that feature is no longer needed.  But, we
    still need an example of how to use multiple roots for actual data, so
    this configuration has been changed to reflect that.

--- core/hooks/pick_environment.py
+++ core/hooks/pick_environment.py
@@ -3,7 +3,6 @@ Copyright (c) 2012 Shotgun Software, Inc
 ----------------------------------------------------
 
 Hook which chooses an environment file to use based on the current context.
-This file is almost always overridden by a standard config.
 
 """
 
@@ -13,17 +12,29 @@ class PickEnvironment(Hook):
 
     def execute(self, context, **kwargs):
         """
-        The default implementation assumes there are two environments, called shot 
-        and asset, and switches to these based on entity type.
+        The default implementation assumes there are three environments, called shot, asset 
+        and project, and switches to these based on entity type.
         """
         
-        # must have an entity
-        if context.entity is None:
+        if context.project is None:
+            # our context is completely empty! 
+            # don't know how to handle this case.
             return None
         
-        if context.entity["type"] == "Shot":
-            return "shot"
-        elif context.entity["type"] == "Asset":
-            return "asset"
+        if context.entity is None:
+            # we have a project but not an entity
+            return "project"
+        
+        if context.entity and context.step is None:
+            # we have an entity but no step!
+            # this is convered by a single environment
+            return "shot_and_asset"
+            
+        if context.entity and context.step:
+            # we have a step and an entity
+            if context.entity["type"] == "Shot":
+                return "shot_step"
+            if context.entity["type"] == "Asset":
+                return "asset_step"            
 
         return None
