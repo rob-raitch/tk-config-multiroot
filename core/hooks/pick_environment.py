7649135e9e5ce86227fa2bd8b6b8a8c09306db45
KP <kp@shotgunsoftware.com>
2014-10-08 15:59:30 -0500

    #26726 Fix reference to non-existent environment When we tightened up our multi-root config to match our standard default config, we missed the case where there is an entity but no step in the context. It used to be handled by a single environment called shot_and_asset, but should match the standard default config and return the environment for the specific entity type if it's a Shot, Asset, or Sequence.

--- core/hooks/pick_environment.py
+++ core/hooks/pick_environment.py
@@ -34,8 +34,12 @@ class PickEnvironment(Hook):
         
         if context.entity and context.step is None:
             # we have an entity but no step!
-            # this is convered by a single environment
-            return "shot_and_asset"
+            if context.entity["type"] == "Shot":
+                return "shot"
+            if context.entity["type"] == "Asset":
+                return "asset"            
+            if context.entity["type"] == "Sequence":
+                return "sequence" 
             
         if context.entity and context.step:
             # we have a step and an entity
